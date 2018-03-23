99e5b493022a9994b0ae422d1dad9ccd47d3c7e8
riversword <2694473261@qq.com>
2018-03-23 23:42:16 +0800

    微信小程序开发学习

---
+++ data/posts-data.js
@@ -0,0 +1,52 @@
+var local_database = [
+  {
+    avatar_img: "/images/avatar/1.png",
+    title: "胆小的人，注定要失去生命中的精彩与美丽",
+    date: "Nov 21 2017",
+    post_img: "/images/post/bl.png",
+    view: 65,
+    collect: 90,
+    content: "离开办公桌，影印一份资料，不过三分钟时间，一只蚂蚁爬上她刚买的黑森林蛋糕。那蛋糕是她的午茶点心，这下享用的兴致全无了。她拿起叉子把蚂蚁取出，然后质问它为什么破坏她的好心情。浑身沾满糖衣的蚂蚁，慢条斯理的回答：\"我饿了，被蛋糕的香味给吸引过来。\"蚂蚁说，它..."
+  },
+  {
+    avatar_img: "/images/avatar/2.png",
+    title: "美丽而精致的冰雪大世界",
+    date: "Oct 11 2017",
+    post_img: "/images/post/cat.png",
+    view: 33,
+    collect: 43,
+    content: "美丽而精致的冰雪大世界哈尔滨的冰雪大世界非常美丽，是我想往已久的地方，是我流连忘返的地方。一个寒冷的周末，我们全家坐一夜火车专程赶往哈尔滨参观冰雪大世界。带着一路的兴奋我们来到了冰雪大世界的门口，大门是用冰做的，上面刻着八个大字：第八届冰雪..."
+  },
+  {
+    avatar_img: "/images/avatar/3.png",
+    title: "剪短发",
+    date: "Dec 20 2017",
+    post_img: "/images/post/crab.png",
+    view: 25,
+    collect: 12,
+    content: "扎辫子可以让女孩显得很可爱。我曾经也有两条小辫子，由于以前扎得太紧，头发容易脱落，慢慢地干燥起来。而且奶奶现在手不好，不方便给我扎头发了。今天，奶奶让我到理发店中把辫子剪掉，剪成短发。理发的还帮我弄了个前流海，我照了一下还蛮可爱。虽然不能..."
+  },
+  {
+    avatar_img: "/images/avatar/4.png",
+    title: "这么短暂，那么永恒",
+    date: "Jan 1 2018",
+    post_img: "/images/post/sls.jpg",
+    view: 25,
+    collect: 23,
+    content: "窗外面，悄然无声，月色朦胧。繁星布满银河，好似一颗颗钻石镶在深蓝色的绸子上，闪烁着光芒。几千几万年来，它们一直在自己的位置闪烁，不曾改变。可是我却从未记清具体某颗星的位置，也不知道它的名字、真正面目、来历。它们是那么平凡，闪耀了几千几..."
+  },
+  {
+    avatar_img: "/images/avatar/5.png",
+    title: "50米短跑",
+    date: "Feb 13 2018",
+    post_img: "/images/post/vr.png",
+    view: 52,
+    collect: 14,
+    content: "50米短跑一听到发令声，就拼命摆臂迈腿的运动是什么？对了，当然是跑步喽！我们暑假一回来上学，第一件事就是体康测试。在四年级体测项目中，有两项是跑步，分别是50米和200米，最激烈的要属50米。50米是一项超短距离运动，最考起跑和爆发力..."
+  }
+];
+
+//定义出口，类似Node.js
+module.exports = {
+  postList: local_database
+};
